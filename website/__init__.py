4a753b52d24faf19612d052a9609463f42e15597
ejbertrand <eddas_email@yahoo.com>
2021-06-05 00:54:11 -0300

    Email configuration

--- website/__init__.py
+++ website/__init__.py
@@ -2,23 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-	# 2. Changed the hierarchy of the views
 	from .views.views import views
 	from .views.auth import auth
 	from .models import User, Note
 	app = Flask(__name__)
-	# 1. Changed the SECRET KEY
 	app.config["SECRET_KEY"] = b'\x95Q\xfe\xbd\x04c7\xf6I\xabR\x86\xb6U6I\xc34k\x13\xf7q\xa8\xd3`3y"\x17\xa6\x1c\xa7'
 	app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
 	db.init_app(app)
+	create_database(app)
+	# Define mail configuration
+	app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+	app.config['MAIL_PORT'] = 465
+	app.config['MAIL_USERNAME'] = 'fifo.bertrand@gmail.com'
+	app.config['MAIL_PASSWORD'] = 'Piano89FIFO'
+	app.config['MAIL_USE_TLS'] = False
+	app.config['MAIL_USE_SSL'] = True
+	mail = Mail(app)
+	msg = Message('Hello', sender = 'fifo.bertrand@gmail.com', recipients = ['eddasjbertrand@gmail.com'])
+	msg.body = "Chancho Chanchia, Chiiiiiii"
+	with app.app_context():
+		mail.send(msg)
+	# Finished defining mail configuration
 	app.register_blueprint(views, url_prefix = '/')
 	app.register_blueprint(auth, url_prefix = '/')
-	create_database(app)
 	login_manager = LoginManager()
 	login_manager.login_view = "auth.login"
 	login_manager.init_app(app)
