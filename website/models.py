23c67ed0c055afdc49b29073fc1a35d69c89e15a
ejbertrand <eddas_email@yahoo.com>
2021-06-06 17:50:14 -0300

    Creating new tables, for services and payment method

--- website/models.py
+++ website/models.py
@@ -2,15 +2,33 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	data = db.Column(db.String(10000))
-	date = db.Column(db.DateTime(timezone = True), default = func.now())
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#class Note(db.Model):
+#	id = db.Column(db.Integer, primary_key = True)
+#	data = db.Column(db.String(10000))
+#	date = db.Column(db.DateTime(timezone = True), default = func.now())
+#	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key = True)
 	email = db.Column(db.String(150), unique = True)
 	password = db.Column(db.String(150))
-	first_name = db.Column(db.String(150))
-	notes = db.relationship('Note')
+	first_name = db.Column(db.String(50)
+#	notes = db.relationship('Note')
+
+class History(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	client_name = db.Column(db.String(250))
+	date = db.Column(db.DateTime(timezone = True), default = func.now())
+	value = db.Column(db.Float)
+	service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+	pay_met_id = db.Column(db.Integer, db.ForeignKey('paymentmethod.id'))
+
+class Service(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	type = db.Column(db.String(150), unique = True)
+	history = db.relationship('History')
+
+class PaymentMethod(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	type = db.Column(db.String(100), unique = True)
+	history = db.relationship('History')
